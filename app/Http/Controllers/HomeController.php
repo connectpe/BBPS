85c970cce7c18c1aa0dd0dcfa020e279b03723b5
Ramanand <ramanand.rajput@groscope.com>
2026-01-24 15:26:23 +0530

    homecontroller model name change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     {
        
         $services = GlobalService::where('is_active', '1')->get();
-        $requestedServices = ServiceRequest::where('user_id', auth()->id())
+        $requestedServices = GlobalService::where('user_id', auth()->id())
             ->get()
             ->keyBy('service_id'); 
 
