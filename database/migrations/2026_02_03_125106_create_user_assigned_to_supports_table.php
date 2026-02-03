3c681cd3e2b9d60cf3a7adf1a52c6094d217446e
Ramanand <ramanand.rajput@connectpe.in>
2026-02-03 13:08:26 +0530

    user assigned table modify add the assigned to column

--- database/migrations/2026_02_03_125106_create_user_assigned_to_supports_table.php
+++ database/migrations/2026_02_03_125106_create_user_assigned_to_supports_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_assigned_to_supports', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('assined_to')->constrained('users')->cascadeOnDelete();
             $table->foreignId('updated_by')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
