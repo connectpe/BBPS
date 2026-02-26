15b64f206f4386d5a2c890cadcd12e303460983c
Ramanand <ramanand.rajput@connectpe.in>
2026-02-26 10:04:45 +0530

    add the method for the initiate the video kyc link using the cashfree

--- app/Http/Controllers/DocumentVerificationController.php
+++ app/Http/Controllers/DocumentVerificationController.php
@@ -6,6 +6,7 @@ use App\Models\BusinessInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 
 class DocumentVerificationController extends Controller
@@ -185,7 +186,7 @@ class DocumentVerificationController extends Controller
 
             if ($response['status'] == true && $response['data']['valid'] == true) {
                 BusinessInfo::where('user_id', $this->userId)->update([
-                    'is_gstin_verify' => 1,
+                    'is_gstin_verify' => '1',
                 ]);
             }
 
@@ -228,7 +229,6 @@ class DocumentVerificationController extends Controller
                 ]);
             }
 
-            dd($response->json());
 
             return response()->json([
                 'status' => true,
@@ -239,6 +239,128 @@ class DocumentVerificationController extends Controller
                 'status' => false,
                 'message' => $e->getMessage(),
             ], 500);
+        }
+    }
+
+    public function verifyIfsc(Request $request){
+        try{
+             $request->validate([
+                
+                'ifsc' => 'required|string',
+            ]);
+
+            $payload = [
+                'ifsc'  => $request->ifsc,
+                'verification_id'=>time(),
+                
+            ];
+            // dd($this->clientSecret);
+            $endpoint = "https://api.cashfree.com/verification/ifsc";
+
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'x-client-id' => $this->clientID,
+                'x-client-secret' => $this->clientSecret,
+            ])->post($endpoint, $payload);
+            // dd($response);
+            return response()->json([
+                'status'=> true,
+                'data'=>$response->json(),
+            ]);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+
+            ],500);
+        }
+    }
+
+    public function createUserToCashFree(Request $request){
+        try{
+           
+
+            $user_id = 'USER'.'11';
+            $date = '2024-12-01';
+            $payload = [
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'phone'=> $request->phone,
+                'address'=> $request->address,
+                'user_id'=> $user_id
+            ];
+            $endpoint = 'https://api.cashfree.com/verification/user';
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'x-client-id' => $this->clientID,
+                'x-client-secret' => $this->clientSecret,
+                'x-api-version' => $date
+            ])->post($endpoint, $payload);
+
+            $finalResponse = $response->json();
+            // dd($finalResponse);
+            $data = [];
+            if($finalResponse['code'] == 'user_id_already_exists'){
+                $data = [
+                    'user_id' => $user_id,
+                    
+                ];
+            }else{
+                $data = [
+                    'user_id' => $finalResponse['user_id'],
+                    
+                ];
+
+            }
+            
+            return $data;
+        }catch(Exception $e){
+            return response()->json([
+                'status'=> false,
+                'message'=> $e->getMessage()
+            ]);
+        }
+    }
+    public function initiateVideoKyc(Request $request){
+        try{
+            $request->validate([
+                'phone'=> 'required',
+                'email'=> 'required',
+                'name'=> 'required',
+                'address'=> 'required'
+            ]);
+            $date = '2024-12-01';
+           
+            $response = $this->createUserToCashFree($request);
+
+            // dd($response);
+
+            $payload = [
+                'verification_id'=> time().round(10000,99999),
+                'user_template'=> 'vkyc_user_template_v1',
+                'user_id'=> $response['user_id'],
+                'notification_types'=> ['whatsapp']
+            ];
+            // dd(1);
+            $endpoint = "https://api.cashfree.com//verification/vkyc";
+            $apiResponse = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'x-client-id' => $this->clientID,
+                'x-client-secret' => $this->clientSecret,
+                'x-api-version' =>$date
+            ])->post($endpoint, $payload);
+            // dd($apiResponse->json());
+            return response()->json([
+                'status'=> true,
+                'message'=> 'Kyc link genratewd successfully',
+                'data'=> $apiResponse->json()
+            ]);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'status'=> false,
+                'message'=> $e->getMessage()
+            ]);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -33,11 +33,13 @@ Route::group(['middleware' => ['logs']], function () {
     Route::post('callback/{type}', [CallbackController::class, 'handle'])->name('api.callback');
 });
 
-Route::group(['middleware'=> ['logs','auth'],'prefix'=>'document'],function(){
+Route::group(['middleware'=> ['logs'],'prefix'=>'document'],function(){
     Route::post('verify-pan',[DocumentVerificationController::class,'panVerify'])->name('pan.verify');
     Route::post('verify-account',[DocumentVerificationController::class,'VerifyAccountDetails'])->name('bank.account.verify');
     Route::post('verify-cin',[DocumentVerificationController::class,'verifyCinNumber'])->name('cin.verify');
     Route::post('verify-gstin',[DocumentVerificationController::class,'verifyGstinNumber'])->name('gstin.verify');
+    Route::post('verify-ifsc',[DocumentVerificationController::class,'verifyIfsc'])->name('verify.ifsc');
+    Route::post('initiate-video-link',[DocumentVerificationController::class,'initiateVideoKyc'])->name('initiate.video.link');
 });
 
 
