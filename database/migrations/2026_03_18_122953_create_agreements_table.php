0c61014adf86c13b50db0e1a8563f94588479fd1
Abhishek Chauhan <abhishek.chauhan@connectpe.in>
2026-03-18 18:25:46 +0530

    Date : 18-03-2026 2. Create an Optimized Migration for the upload Agreement. 3. Created a Laravel Model for the Agreement.

---
+++ database/migrations/2026_03_18_122953_create_agreements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agreements', function (Blueprint $table) {
+            $table->id();
+            $table->text('file_path');
+            $table->enum('status', ['0', '1'])->default('1');
+            $table->foreignId('updated_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agreements');
+    }
+};
