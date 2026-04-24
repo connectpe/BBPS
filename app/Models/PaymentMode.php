d669eea49dfc983b9ef3bc133a2c97638b7bcb17
Ramanand <ramanand.rajput@groscope.com>
2026-04-24 10:26:45 +0530

    update PaymentMode model

--- app/Models/PaymentMode.php
+++ app/Models/PaymentMode.php
@@ -6,15 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentMode extends Model
 {
-    protected $fillable = [
-        'mode_id',
-        'service_id',
-        'mode_name',
-        'mode_slug',
-        'min_order_value',
-        'max_order_value',
-        'tax_value',
-        'is_active',
-        'updated_by',
-    ];
+    protected $fillable = ['mode_id', 'service_id', 'mode_name', 'mode_slug', 'min_order_value', 'max_order_value', 'tax_value', 'is_active', 'updated_by',];
 }
